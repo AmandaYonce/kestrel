490735449d361f094821723bbe2c25fbfb44c1a1
Amanda Yonce <amanda.stork@gmail.com>
2020-02-28 07:53:50 -0500

    added delete user button

--- src/react/components/Account/AccountMain.js
+++ src/react/components/Account/AccountMain.js
@@ -3,6 +3,8 @@ import {
     CardBody,
     CardTitle, CardSubtitle, CardText
   } from 'reactstrap';
+  import SmallIcon from "../../../images/owl-black-square-single.png"
+  import Button from 'react-bootstrap/Button'
 
 
 class AccountMain extends Component {
@@ -14,6 +16,10 @@ class AccountMain extends Component {
         <CardSubtitle className="text-secondary mb-3 font-weight-normal text-uppercase" style={{ fontSize: '0.8rem' }}>Subtitle- maybe user about section</CardSubtitle>
         <CardText className="text-secondary mb-4 font-weight-normal" style={{ fontSize: '0.95rem' }}>do we want a button for delete user? What about edit or update user?</CardText>
       </CardBody>
+      <Button variant="secondary" type="submit"  style={{ "fontSize": "28", "backgroundColor": "red" }}>
+          <img src={SmallIcon} alt="avatar" className="img-fluid rounded-circle" style={{ "width": "40px", "paddingRight": "5px" }} />
+            Delete User
+          </Button>
       </Fragment>
     );
   }
