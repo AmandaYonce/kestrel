ba333d8a28243fd85c1898935c43315c7a7057d0
Amanda Yonce <amanda.stork@gmail.com>
2020-03-19 13:05:41 -0400

    removed login folder as duplicate

--- src/react/components/index.js
+++ src/react/components/index.js
@@ -1,6 +1,5 @@
-export { default as LoginForm } from "./LoginForm/LoginForm";
-export { default as Menu } from "./Menu/Menu";
 
+export { default as Menu } from "./Menu/Menu";
 export {default as AccountMain} from "./Account/AccountMain"
 export {default as AccountPhoto} from "./Account/AccountPhoto"
 export {default as NewMessage} from "./MessageFeed/NewMessage"
